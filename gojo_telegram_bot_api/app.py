ef058c5beff3f6e597bbdca39e18f6b29b1d37b3
matt468779 <mengistusima@gmail.com>
2023-09-05 11:05:39 +0300

    feature: changed from flask to quart for async

--- gojo_telegram_bot_api/app.py
+++ gojo_telegram_bot_api/app.py
@@ -1,24 +1,28 @@
-from flask import Flask
+import quart.flask_patch
+import asyncio
+import hypercorn.asyncio
+from pyngrok import ngrok
+from quart import Quart, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 from pyngrok import ngrok
 from config import Config
 from message_parser import Update
 from telegram_bot import TelegramBot
-from flask import request
 from openai_helper import OpenAiHelper
 
-app = Flask(__name__)
+app = Quart(__name__)
 app.config.from_object(os.getenv('APP_SETTINGS'))
 
 db = SQLAlchemy(app)
 
 @app.route('/', methods=["GET", "POST"])
-def handle_webhook():
-    update = Update(**request.json)
+async def handle_webhook():
+    update = Update(**await request.json)
     chat_id = update.message.chat.id
-
-    response = app.openai_helper.get_response(update.message.text)
+    # history = await app.bot.get_chat_history(chat_id)
+    # print(history)
+    response = app.openai_helper.get_response(update.message.text)    
     app.bot.send_message(chat_id, response)
 
     return "OK", 200
@@ -27,13 +31,18 @@ def run_ngrok(port=8000):
     http_tunnel = ngrok.connect(port)
     return http_tunnel.public_url
 
-app.bot = TelegramBot(Config.TELEGRAM_TOKEN)
-host = run_ngrok(Config.PORT)
-app.bot.set_webhook(host)
-app.openai_helper = OpenAiHelper(Config.OPENAI_TOKEN)
+@app.before_serving
+async def startup():
+    app.bot = TelegramBot(Config.TELEGRAM_TOKEN)    
+    ngrok.set_auth_token(os.getenv("NGROK_AUTH_TOKEN"))
+    host = run_ngrok(Config.PORT)
+    app.bot.set_webhook(host)
+    app.openai_helper = OpenAiHelper(Config.OPENAI_TOKEN)
 
-def main():
-    app.run(port=Config.PORT, debug=True, use_reloader=False)
+async def main():
+    quart_cfg = hypercorn.Config()
+    quart_cfg.bind = [f"127.0.0.1:{Config.PORT}"]
+    await hypercorn.asyncio.serve(app, quart_cfg)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- gojo_telegram_bot_api/telegram_bot.py
+++ gojo_telegram_bot_api/telegram_bot.py
@@ -7,7 +7,7 @@ class TelegramBot:
         self.bot_api_url = f"{Config.TELEGRAM_API}/bot{self.token}"
 
     def set_webhook(self, host):
-        host = host.replace("http", "https")
+        # host = host.replace("http", "https")
         set_webhook_url = f"{self.bot_api_url}/setWebhook?url={host}"
         response = requests.get(set_webhook_url)
         response.raise_for_status()
@@ -17,3 +17,6 @@ class TelegramBot:
         response = requests.post(send_message_url, json={"chat_id": chat_id,
                                                           "text": message})
         response.raise_for_status()
+
+    async def get_chat_history(self, chat_id, limit=30):
+        return []
